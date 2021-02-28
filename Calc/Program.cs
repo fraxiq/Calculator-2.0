9a037a1dd5cb12300ac86ce33e21cd6b577e986a
fraxiq <alexns007@abv.bg>
2021-02-28 17:27:54 +0200

    fixed directory structure

---
+++ Calc/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            double result = Calculator(num1, num2, operation);
+            Console.WriteLine(result);
+
+        }
+
+        private static double Calculator(double num1, double num2,
+            string operation)
+        {
+            if (operation == "+")
+            {
+                return Add(num1, num2);
+            }
+            else if (operation == "-")
+            {
+                return Subtract(num1, num2);
+            }
+            else if (operation == "*")
+            {
+                return Multiply(num1, num2);
+            }
+            else if (operation == "/")
+            {
+                return Divide(num1, num2);
+            }
+
+            return 0.0;
+        }
+    }
+}
